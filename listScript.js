6586cb6c1af983f6a3b1cdee86f101cd31384132
Hung Truong <hungtruong@csus.edu>
2020-11-25 09:33:57 -0800

    Update listScript.js

--- listScript.js
+++ listScript.js
@@ -1,5 +1,5 @@
 function filterTable() {
-  var input, filter, table, tr, td, i, txtValue,txtValue2;
+  var input, filter, table, tr, td, i, txtValue,txtValue2, txtValue3, txtValue4;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
   table = document.getElementById("myTable");
@@ -7,11 +7,17 @@ function filterTable() {
   for (i = 0; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("td")[0];
     td2 = tr[i].getElementsByTagName("td")[1];
-    if (td || td2) {
+    td3 = tr[i].getElementsByTagName("td")[2];
+    td4 = tr[i].getElementsByTagName("td")[3];
+    if (td || td2 || td3) {
       txtValue = td.textContent || td.innerText;
       txtValue2 = td2.textContent || td2.innerText;
+      txtValue3 = td3.textContent || td3.innerText;
+      txtValue4 = td4.textContent || td4.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1 ||
-          txtValue2.toUpperCase().indexOf(filter) > -1) {
+          txtValue2.toUpperCase().indexOf(filter) > -1 ||
+          txtValue3.toUpperCase().indexOf(filter) > -1 ||
+          txtValue4.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
       }else {
         tr[i].style.display = "none";
